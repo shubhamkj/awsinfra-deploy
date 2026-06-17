6ee27801cc60f7ce04f9178867d2407d24b4e571
Shubham Jain <Shubham9291@gmail.com>
2026-06-17 13:11:46 +0530

    Update GitHub OAuth token in CodePipeline source stage

--- awsinfra_deploy/awsinfra_deploy_stack.py
+++ awsinfra_deploy/awsinfra_deploy_stack.py
@@ -59,7 +59,7 @@ class AwsinfraDeployStack(Stack):
             owner='shubhamkj',  # Replace with your GitHub username
             repo='awsinfra-deploy',  # Replace with your repository name
             branch='master',
-            oauth_token=SecretValue.secrets_manager('cdk-infra-june26'),
+            oauth_token=SecretValue.secrets_manager('github-token1'),
             output=source_output
         )
 
